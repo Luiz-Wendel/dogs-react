a416a2082d4f4ca270b24f24ded0dd66f1e0d4bd
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-08-16 11:49:13 -0300

    Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './styles.module.css';
+
+// Images
+import { ReactComponent as Dogs } from '../../assets/img/dogs.svg';
 
 const Header = () => {
   return (
-    <nav className="container">
-      <ul>
-        <li>Home</li>
-        <li>Login</li>
-      </ul>
-    </nav>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Signup
+        </Link>
+      </nav>
+    </header>
   );
 };
 
