cc28796b547f1518c9bb99c9a3fb643175372c00
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-08-19 15:30:39 -0300

    Fix error

--- src/components/FeedModal/index.js
+++ src/components/FeedModal/index.js
@@ -27,7 +27,7 @@ const FeedModal = ({ photo, setModalPhoto }) => {
 
   return (
     <div className={styles.modal} onClick={handleOutsideClick}>
-      {error && <Error error={error} />}
+      <Error error={error} />
       {loading && <Loading />}
       {data && <PhotoContent data={data} />}
     </div>
