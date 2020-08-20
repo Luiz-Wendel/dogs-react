cf26d2d8c995964c5971e308456bd10c083e5599
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-08-20 16:36:56 -0300

    Victory (Graph library)

--- src/pages/User/Statistics/index.js
+++ src/pages/User/Statistics/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
+// API
+import { STATS_GET } from '../../../api';
+
+// Custom Hooks
+import useFetch from '../../../hooks/useFetch';
+
+// Components
+import Head from '../../../components/Head';
+import Loading from '../../../components/Loading';
+import Error from '../../../components/Error';
+import StatisticGraphs from '../../../components/StatisticsGraphs';
+
 const Statistics = () => {
-  return (
-    <div>
-      <h1 className="title">Statistics</h1>
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Statistics" />
+        <StatisticGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default Statistics;
