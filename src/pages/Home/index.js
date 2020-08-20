377e0d3b036e6da3a471834e18e6f513e108bcf3
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-08-20 15:08:27 -0300

    Head component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,9 +3,13 @@ import React from 'react';
 // Pages
 import Feed from '../Feed';
 
+// Components
+import Head from '../../components/Head';
+
 const Home = () => {
   return (
     <section className="container mainContainer">
+      <Head title="Photos" description="Homepage of Dogs, with photo feed." />
       <Feed />
     </section>
   );
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,12 +4,16 @@ import { useParams } from 'react-router-dom';
 // Pages
 import Feed from '../Feed';
 
+// Components
+import Head from '../../components/Head';
+
 const Profile = () => {
   const { user } = useParams();
 
   return (
     <section className="container mainContainer">
       <h1 className="title">{user}</h1>
+      <Head title={user} />
       <Feed user={user} />
     </section>
   );
